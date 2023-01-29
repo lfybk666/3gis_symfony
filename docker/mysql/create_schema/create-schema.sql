e94fb6fec0854db009bd774bccec555c198f3146
lfybk666 <danil-sheptalin@mail.ru>
2023-01-29 22:47:43 +0700

    start 3gis and build docker

---
+++ docker/mysql/create_schema/create-schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE 3gis;
+
+-- use 3gis;
+--
+-- CREATE TABLE firms (
+--     id int not null,
+--     title varchar not null,
+--     primary key (id)
+-- );
+
+-- CREATE TABLE buildings (
+--
+-- );
+-- CREATE TABLE headings (
+--
+-- );
+-- CREATE TABLE users (
+--
+-- );
+
---
+++ src/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$connection = mysqli_connect('mysql', 'root', 'root');
+
+echo '<pre>';
+var_dump($connection);
